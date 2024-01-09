d1fd52f28cf1f26fe3319d671b54e068a73b7e72
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-01-09 20:55:46 +0530

    class practise

--- crudexp/crud.js
+++ crudexp/crud.js
@@ -4,57 +4,75 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var db = require('./db.js');
 
-db.connect(function(err) {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
 
-    console.log('Connected to database');
-});
 app.use(session({secret:"123!@#"}));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/",function(req,res){
-    res.render('home')
+app.get("/", (req, res) => {
+    res.render("home", { login : "login" });
 });
 
-app.get('/listemp', function(req, res) {
-    db.query('SELECT * FROM emp', function(err, result) {
-        if (err) {
-            console.error('Error querying the database:', err);
-            return;
+
+app.get("/login", (req, res) => {
+    req.session.msg = "enter details to login";
+    res.render('login', { msg: req.session.msg });
+});
+
+app.post("/login_submit", (req, res) => {
+    var { email, password } = req.body;
+    let sql = `SELECT * FROM users WHERE email=? AND password=?`;
+
+    db.query(sql, [email, password], (err, results) => {
+
+        if (results.length > 0) {
+            req.session.user_id = results[0].user_id;
+            res.render('home',{login:"logout"});
         }
-
-        console.log('Result from the database:', result); // Add this line to log the result
-
-        res.render('listemp', { result: result });
-    });
-});
-
-
-
-app.get('/addemp',function(req,res){
-    db.query("select * from dept",function(err,result){
-        res.render('addemp',{result:result});
-    })
-});
-
-
-app.post('/addemp_submit', function(req, res) {
-    let { ename, add1, add2, dob, salary, deptid } = req.body;
-    let cdate = new Date();
-    let month = cdate.getMonth() + 1;
-    let doj = cdate.getFullYear() + "-" + month + "-" + cdate.getDate();
-    let sql = "INSERT INTO emp (ename, add1, add2, dob, doj, salary, deptid) VALUES ('" + ename + "','" + add1 + "','" + add2 + "','" + dob + "','" + doj + "','" + salary + "','" + deptid + "')";
-
-    db.query(sql, function(err, result) {
-        if (result.insertId > 0) {
-            res.redirect('/listemp');
+        
+        else {
+            req.session.msg = "Username and password do not match";
+            res.render('login', { msg: req.session.msg });
         }
     });
 });
+
+
+// app.get('/listemp', function(req, res) {
+//     db.query('SELECT * FROM emp', function(err, result) {
+//         if (err) {
+//             console.error('Error querying the database:', err);
+//             return;
+//         }
+
+//         console.log('Result from the database:', result); // Add this line to log the result
+
+//         res.render('listemp', { result: result });
+//     });
+// });
+
+
+
+// app.get('/addemp',function(req,res){
+//     db.query("select * from dept",function(err,result){
+//         res.render('addemp',{result:result});
+//     })
+// });
+
+
+// app.post('/addemp_submit', function(req, res) {
+//     let { ename, add1, add2, dob, salary, deptid } = req.body;
+//     let cdate = new Date();
+//     let month = cdate.getMonth() + 1;
+//     let doj = cdate.getFullYear() + "-" + month + "-" + cdate.getDate();
+//     let sql = "INSERT INTO emp (ename, add1, add2, dob, doj, salary, deptid) VALUES ('" + ename + "','" + add1 + "','" + add2 + "','" + dob + "','" + doj + "','" + salary + "','" + deptid + "')";
+
+//     db.query(sql, function(err, result) {
+//         if (result.insertId > 0) {
+//             res.redirect('/listemp');
+//         }
+//     });
+// });
 
 
 
